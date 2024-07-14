49d5705576968b1ef75a58637ee5d6f605451737
MadRobin13 <madrobin1340@gmail.com>
2024-07-13 23:48:36 -0400

    added files and key

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import {token} from './key.js';
+
+const {Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.login(token);
