d1ca790f4771ee1228a033de84d9926e113a77fb
MadRobin13 <madrobin1340@gmail.com>
2024-07-14 00:54:24 -0400

    fixed login with readfile for key

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import {token} from './key.js';
-
 const {Client, IntentsBitField} = require('discord.js');
-
+const fs = require('fs');
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
+        IntentsBitField.Flags.MessageContent
+    ]});
+
+fs.readFile('src/key.txt', (err, data) => {
+    if (err) {
+        throw(err);
+    }
+    else {
+        const key = data.toString();
+        client.login(key);
+        console.log("logged in!")
+        }
 });
 
-client.login(token);
+// const reader = new FileReader();
+// reader.readAsText('./key.txt', 'uft-8');
+// const key = reader.result.toString();
+
