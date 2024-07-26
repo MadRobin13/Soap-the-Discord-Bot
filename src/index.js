6d8a86739af16a84f970aa80fa39e21019e0959f
MadRobin13 <madrobin1340@gmail.com>
2024-07-26 17:09:01 -0400

    formatting and comments

--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
+/*
+
+Name: Soap the Discord Bot
+Author: Abhimanyu Chaudhary
+Date: 2024-07-26
+Description: A discord bot that detects and removes any messages in a 
+             Discord server containing swear words or profanity. 
+             It logs actions and reprimands the user who sent the message.
+
+*/
+
+//########################################################################################
+
+
+// This is where we import the necessary modules and packages
+
 const dotenv = require('dotenv').config();
 const {Client, IntentsBitField} = require('discord.js');
 const fs = require('fs');
 
 
+
+// This is where we declare the swearWords array
+
 let swearWords = [];
 
-fs.readFile('src/words.txt', 'utf8', async (err, data) => {
+
+
+// This is where we read the words.txt file and 
+// populate the swearWords array with the words
+// contained in the file
+
+fs.readFile('src/words.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('error reading file', err);
         return;
     }
-   
     swearWords = data.split('\r\n');
+
+
+
+// We immediately call the loginClient function 
+// to log in the bot to Discord but only after the 
+// swearWords array has been populated
+
     loginClient();
 });
+
+
+
+// Here we define the specific intents that the bot will
+// use to interact with the Discord server
 
 const client = new Client({
     intents: [
@@ -23,12 +59,24 @@ const client = new Client({
         IntentsBitField.Flags.MessageContent 
     ]});
 
+
+
+// Here we define the ready event listener to
+// log a message that the bot is ready to be used
+
 client.on('ready', (c) => {
     console.log(`${c.user.username} is ready!`)
 });
 
 
+
+// Here we define the messageCreate event listener to
+// check if a message was sent by a user, and 
+// to split the message up into words and check if
+// any of the words are swear words
+
 client.on('messageCreate', async (message) => {
+
 
     if (message.author.bot) return;
 
@@ -50,6 +98,11 @@ client.on('messageCreate', async (message) => {
     }
 
 
+    
+// If the message contains a swear word, the bot will
+// reply to the user who sent the message
+// and delete the original message
+
     if (containsSwearWord) {
         try {
             await message.reply(`Don't swear here ${message.author}!`);
@@ -62,6 +115,8 @@ client.on('messageCreate', async (message) => {
 
 
 
+// Here we define the loginClient function to 
+// log in the bot to Discord
 function loginClient() {
     client.login(process.env.TOKEN);
 }
